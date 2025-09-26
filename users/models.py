76e5ae8c1d5a53f89c8e7b5d2416db4c87b17f6a
alexander vagin <sashavagin1987@gmail.com>
2025-09-26 21:58:37 +1000

    Настроена модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Уажите почту"
+    )
+    avatar = models.ImageField(
+        upload_to="users/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар",
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Номер телефона",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=50,
+        verbose_name="Город",
+        blank=True,
+        null=True,
+        help_text="Введите город в котором вы живете",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
