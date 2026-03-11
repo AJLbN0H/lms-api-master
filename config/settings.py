e1280c8e4341d487c496960c54dd68386c6fa341
AJLbN0H <sashavagin1987@gmail.com>
2026-03-11 22:35:15 +1000

    Docker добавлен

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-0y7qw-z&sl)a-+_!z-#8mb2ts(c)b3t=fo@lwr#bb&lnc#_!(p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
